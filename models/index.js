c4516bafe19fbc833621fad261a1504c61672c93
BenPaulat <benjamin.d.paulat@gmail.com>
2021-11-28 14:38:01 -0600

    added foreign key relations for classes in index file

--- models/index.js
+++ models/index.js
@@ -2,3 +2,14 @@ const Department = require('./department');
 const Timecard = require('./timecard');
 const Individual = require('./individual');
 
+Department.hasMany(Individual, {
+    foreignKey: 'department_id'
+});
+
+Individual.belongsTo(Department, {
+    foreignKey: 'department_id'
+});
+
+Timecard.belongsTo(Individual, {
+    foreignKey: 'individual_id'
+});
