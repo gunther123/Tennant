9756dc3525d5c27dcd1001ed9046119f08f892a2
Steven Slocum <steven.t.slocum@gmail.com>
2021-11-30 11:15:33 -0600

    created GET route to get all individuals in db

--- controllers/api/individual-routes.js
+++ controllers/api/individual-routes.js
@@ -1,5 +1,15 @@
 const router = require('express').Router();
 const { Individual } = require('../../models');
 
+// Get all Timecards
+router.get('/', (req, res) => {
+    // Find all Timecards
+    Individual.findAll()
+      .then(dbGetData => res.json(dbGetData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
 
 module.exports = router;
