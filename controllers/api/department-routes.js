ba71cda336649d9d231ca6b07e318ebdbd90a6c9
Steven Slocum <stevesl23@hotmail.com>
2021-11-29 23:45:06 -0600

    added get route for specific dept id

--- controllers/api/department-routes.js
+++ controllers/api/department-routes.js
@@ -1,17 +1,31 @@
 const router = require('express').Router();
 const { Department } = require('../../models');
 
+// Get all Dept's Route
 router.get('/', (req, res) => {
     // Find all Department's
-    Department.findAll({
-      include: [
-        {
-          model: Department,
-          attributes: ['id', 'name', 'deleted']
-        }
-      ]
-    })
+    Department.findAll()
       .then(dbGetData => res.json(dbGetData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+//Get specific Dept Route
+router.get('/:id', (req, res) => {
+    Department.findOne({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbGetData => {
+        if (!dbGetData) {
+          res.status(404).json({ message: 'No department found'}); 
+          return; 
+        }
+        res.json(dbGetData);
+      })
       .catch(err => {
         console.log(err);
         res.status(500).json(err);
