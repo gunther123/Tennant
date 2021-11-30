0e754e88b96d7d934b72f357cb5720a943f0c06f
Steven Slocum <stevesl23@hotmail.com>
2021-11-29 23:31:45 -0600

    added get all departments route

--- controllers/api/department-routes.js
+++ controllers/api/department-routes.js
@@ -1,4 +1,21 @@
 const router = require('express').Router();
 const { Department } = require('../../models');
 
+router.get('/', (req, res) => {
+    // Find all Department's
+    Department.findAll({
+      include: [
+        {
+          model: Department,
+          attributes: ['id', 'name', 'deleted']
+        }
+      ]
+    })
+      .then(dbGetData => res.json(dbGetData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
 module.exports = router;
