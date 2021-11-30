22313dbb44f1767393a284962eea41b23f83a2f3
Steven Slocum <stevesl23@hotmail.com>
2021-11-30 01:24:57 -0600

    created route to GET all Timecards

--- controllers/api/timecard-routes.js
+++ controllers/api/timecard-routes.js
@@ -1,4 +1,16 @@
 const router = require('express').Router();
 const { Timecard } = require('../../models');
 
+// Get all Dept's Route
+router.get('/', (req, res) => {
+    // Find all Timecards
+    Timecard.findAll()
+      .then(dbGetData => res.json(dbGetData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+
 module.exports = router;
