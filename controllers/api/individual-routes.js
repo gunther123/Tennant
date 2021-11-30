79192a4243da6da4d6b32943b86242d0c7d81592
Steven Slocum <stevesl23@hotmail.com>
2021-11-30 01:47:53 -0600

    created GET specific timecard

--- controllers/api/individual-routes.js
+++ controllers/api/individual-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
 const { Individual } = require('../../models');
 
+
 module.exports = router;
--- controllers/api/timecard-routes.js
+++ controllers/api/timecard-routes.js
@@ -1,11 +1,31 @@
 const router = require('express').Router();
 const { Timecard } = require('../../models');
 
-// Get all Dept's Route
+// Get all Timecards
 router.get('/', (req, res) => {
     // Find all Timecards
     Timecard.findAll()
       .then(dbGetData => res.json(dbGetData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+//Get specific Timecard
+router.get('/:id', (req, res) => {
+    Department.findOne({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbGetData => {
+        if (!dbGetData) {
+          res.status(404).json({ message: 'No timecard found'}); 
+          return; 
+        }
+        res.json(dbGetData);
+      })
       .catch(err => {
         console.log(err);
         res.status(500).json(err);
