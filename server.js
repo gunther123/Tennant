839cf855c9b299a06219892e4c535032ee3319bb
Shawn McHenry <drraccoony@gmail.com>
2021-11-30 19:50:42 -0600

    Added base pages

--- server.js
+++ server.js
@@ -1,14 +1,33 @@
 //Require all node modules
 const path = require('path');
 const routes = require('./controllers/');
-const express = require('express');
 const sequelize = require('./config/connection')
-// const exphbs = require('express-handlebars');
-// const hbs = exphbs.create({})
+var express = require('express');
+const app = express();
 
 //Middleware setup
-const app = express();
 const PORT = process.env.PORT || 3001;
+
+//Load the handlebars module
+var exphbs = require('express-handlebars');
+var hbs = exphbs.create({ /* config */ });
+
+// Register `hbs.engine` with the Express app.
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+app.get('/timecards', function (req, res) {
+    res.render('timecards');
+});
+app.get('/departments', function (req, res) {
+    res.render('departments');
+});
+app.get('/people', function (req, res) {
+    res.render('people');
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -17,8 +36,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-//Start server
-app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}!`);
+    //Start server
+    app.listen(PORT, () => {
+        console.log(`*** Tennant Running! ***\n🚀 App listening on http://localhost:${PORT}!\n*********************`);
     })
 });
---
+++ views/departments.handlebars
@@ -0,0 +1,1 @@
+<h1>Departments</h1>
