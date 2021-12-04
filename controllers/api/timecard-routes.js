6c813c6ab11ca894f6b4262155508ebd7a37fa21
Steven Slocum <steven.t.slocum@gmail.com>
2021-12-04 00:39:14 -0600

    added route to get all timecards by individual id

--- controllers/api/timecard-routes.js
+++ controllers/api/timecard-routes.js
@@ -36,12 +36,30 @@ router.get('/:id', (req, res) => {
       });
   });
 
+  // Get all Timecards
+router.get('/individual/:id', (req, res) => {
+  // Find all Timecards
+  Timecard.findAll({
+    where: {
+      deleted: false,
+      individual_id: req.params.id 
+    }
+  })
+    .then(dbGetData => res.json(dbGetData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 //Create new Timecard
 router.post('/', (req, res) => {
+  
   Timecard.create({
     title: req.body.title,
     hours: req.body.hours,
     notes: req.body.notes,
+    date: req.body.date,
     individual_id: req.body.individual_id
     })
     .then(dbPostData => res.json(dbPostData))
