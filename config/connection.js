6e69e73941c7b77bd8a260147631f75051645e01
Shawn McHenry <drraccoony@gmail.com>
2021-12-04 21:20:06 -0600

    Fixed HOST to a ENV variable

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
+    host: process.env.DB_HOST,
     dialect: 'mysql',
     port: 3306
 })
