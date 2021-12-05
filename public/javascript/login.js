5999ed835e79d7d73fcbd175b316f2432f296a03
Carrington5 <88473030+Carrington5@users.noreply.github.com>
2021-12-05 15:34:11 -0600

    added session to server.js file

---
+++ public/javascript/login.js
@@ -0,0 +1,27 @@
+  async function loginHandler(event) {
+    event.preventDefault();
+  
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+  
+    if ( email && password) {
+      const response = await fetch('/api/indivduals/login', {
+        method: 'post',
+        body: JSON.stringify({
+          email,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  }
+  
+  document.querySelector('.login-form').addEventListener('submit', loginHandler);
+  
+  
--- server.js
+++ server.js
@@ -2,9 +2,23 @@
 const path = require('path');
 const routes = require('./controllers/');
 const sequelize = require('./config/connection')
+const session = require('express-session')
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+require('dotenv').config();
+
 var express = require('express');
 const app = express();
 const { Individual, Department, Timecard } = require('./models');
+
+const sess = {
+  secret: process.env.SESS_PW,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
 //Middleware setup
 const PORT = process.env.PORT || 3001;
@@ -20,6 +34,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
 
 app.use(routes);
 
