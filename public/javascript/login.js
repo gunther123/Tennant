1754ccaa4334fa9749e22404ac2465e009c508c0
Carrington5 <88473030+Carrington5@users.noreply.github.com>
2021-12-05 18:48:42 -0600

    added logout button

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,8 +1,8 @@
   async function loginHandler(event) {
     event.preventDefault();
   
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
   
     if ( email && password) {
       const response = await fetch('/api/indivduals/login', {
--- server.js
+++ server.js
@@ -35,6 +35,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session(sess));
+//return session detail to handlebars
+app.use(function (req, res, next) {
+    res.locals.session = req.session;
+    next();
+});
 
 app.use(routes);
 
