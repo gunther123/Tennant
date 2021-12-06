ffd4ee166d395ffc86935e09782cca6333059b10
Carrington5 <88473030+Carrington5@users.noreply.github.com>
2021-12-05 20:45:09 -0600

    linting

--- controllers/HTML/department-routes.js
+++ controllers/HTML/department-routes.js
@@ -2,54 +2,54 @@ const router = require('express').Router();
 const { Department } = require('../../models');
 
 router.get('/', function (req, res) {
-    // Find all Department's
-    Department.findAll()
-      .then(dbGetData => {
-        res.render('departments', { department: dbGetData })
-        //console.log(dbGetData)
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  
-  });
-  router.get('/new/', function (req, res) {
-    res.render('departments/new', {});
-  });
-  
-  router.get('/view/:id', function (req, res) {
-    // Get single department
-    Department.findOne({
-      where: {
-        id: req.params.id
-      }
+  // Find all Department's
+  Department.findAll()
+    .then(dbGetData => {
+      res.render('departments', { department: dbGetData })
+      //console.log(dbGetData)
     })
-      .then(dbGetData => {
-        res.render('departments/view', { department: dbGetData.dataValues })
-        //console.log(dbGetData)
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
-  
-  router.get('/edit/:id', function (req, res) {
-    // Get single department
-    Department.findOne({
-      where: {
-        id: req.params.id
-      }
-    })
-      .then(dbGetData => {
-        res.render('departments/edit', { department: dbGetData.dataValues })
-        //console.log(dbGetData)
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 
-  module.exports = router;
+});
+router.get('/new/', function (req, res) {
+  res.render('departments/new', {});
+});
+
+router.get('/view/:id', function (req, res) {
+  // Get single department
+  Department.findOne({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbGetData => {
+      res.render('departments/view', { department: dbGetData.dataValues })
+      //console.log(dbGetData)
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get('/edit/:id', function (req, res) {
+  // Get single department
+  Department.findOne({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbGetData => {
+      res.render('departments/edit', { department: dbGetData.dataValues })
+      //console.log(dbGetData)
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
--- controllers/HTML/timecard-routes.js
+++ controllers/HTML/timecard-routes.js
@@ -2,60 +2,60 @@ const router = require('express').Router();
 const { Timecard, Individual } = require('../../models');
 
 router.get('/', function (req, res) {
-    // Find all Department's
-    Timecard.findAll()
-      .then(dbGetData => {
-        res.render('timecards', { timecard: dbGetData.reverse() })
-        //console.log(dbGetData)
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
-  router.get('/new', function (req, res) {
-    Individual.findAll()
-      .then(dbGetData => {
-        res.render('timecards/new', { people: dbGetData })
-        //console.log(dbGetData)
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-    //res.render('timecards/new');
-  });
-  router.get('/view/:id', function (req, res) {
-    // Get single timecard
-    Timecard.findOne({
-      where: {
-        id: req.params.id
-      }
+  // Find all Department's
+  Timecard.findAll()
+    .then(dbGetData => {
+      res.render('timecards', { timecard: dbGetData.reverse() })
+      //console.log(dbGetData)
     })
-      .then(dbGetData => {
-        res.render('timecards/view', { timecard: dbGetData.dataValues })
-        //console.log(dbGetData)
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
-  router.get('/edit/:id', function (req, res) {
-    // Get single timecard
-    Timecard.findOne({
-      where: {
-        id: req.params.id
-      }
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+router.get('/new', function (req, res) {
+  Individual.findAll()
+    .then(dbGetData => {
+      res.render('timecards/new', { people: dbGetData })
+      //console.log(dbGetData)
     })
-      .then(dbGetData => {
-        res.render('timecards/edit', { timecard: dbGetData.dataValues })
-        //console.log(dbGetData)
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+  //res.render('timecards/new');
+});
+router.get('/view/:id', function (req, res) {
+  // Get single timecard
+  Timecard.findOne({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbGetData => {
+      res.render('timecards/view', { timecard: dbGetData.dataValues })
+      //console.log(dbGetData)
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+router.get('/edit/:id', function (req, res) {
+  // Get single timecard
+  Timecard.findOne({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbGetData => {
+      res.render('timecards/edit', { timecard: dbGetData.dataValues })
+      //console.log(dbGetData)
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
