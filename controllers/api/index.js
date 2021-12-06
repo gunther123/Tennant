872eddc5e9a154a7739a1fcdeda9bb0d43007ac5
Carrington5 <88473030+Carrington5@users.noreply.github.com>
2021-12-05 18:57:38 -0600

    made changes to index.js, individual-routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,6 +3,17 @@ const departmentRoutes = require('./department-routes');
 const individualRoutes = require('./individual-routes');
 const timecardRoutes = require('./timecard-routes');
 
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    }
+    else {
+      res.status(404).end();
+    }
+  })
+
 router.use('/departments', departmentRoutes);
 router.use('/individuals', individualRoutes);
 router.use('/timecards', timecardRoutes);
